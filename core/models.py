78daa5a496fc521a92c38bd85aa6565e2beb1dcf
or <orduani@gmail.com>
2016-09-05 00:52:56 +0300

    Added views

--- core/models.py
+++ core/models.py
@@ -1,7 +1,9 @@
 import uuid
+from datetime import timedelta
 
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
 
 class BaseModel(models.Model):
@@ -14,14 +16,47 @@ class BroType(BaseModel):
     name = models.TextField()  # The to show to the users
     code = models.TextField()  # When we have to associate it with inner app tasks(like file names, css classes etc)
 
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'code': self.code,
+        }
+
 
 class Message(BaseModel):
     """A bro message from one user to anther"""
+    created_at = models.DateTimeField(auto_now_add=True)
     sender = models.ForeignKey(User, related_name='sender')
     receiver = models.ForeignKey(User, related_name='receiver')
     bro = models.ForeignKey(BroType)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'sender': self.sender_id,
+            'receiver': self.receiver_id,
+            'bro': self.bro.to_dict(),
+        }
+
+    @staticmethod
+    def get_message_from_n_date(days):
+        """Returns QuerySet object with all the messages from last n days"""
+        time_delta = timezone.now() - timedelta(days=days)
+        return Message.objects.filter(created_at__gte=time_delta)
+
+    @staticmethod
+    def get_messages_from_last_day():
+        """Returns QuerySet object with all the messages from last day"""
+        return Message.get_message_from_n_date(1)
+
+    @staticmethod
+    def get_messages_from_last_week():
+        """Returns QuerySet object with all the messages from last week"""
+        return Message.get_message_from_n_date(7)
 
 
 class UserProfile(BaseModel):
     """Additional info for the User model"""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bros = models.ManyToManyField(BroType)
--- core/views.py
+++ core/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from core.models import Message
+from django.core.paginator import Paginator
+from django.http import JsonResponse
 
-# Create your views here.
+
+def get_messages(request):
+    """Returns last n messages for specific user"""
+    user = request.user  # TODO make sure the user is token authnticated
+    page = request.GET.get('page', 1)
+    paginator = Paginator(Message.objects.filter(receiver=user), 40)
+
+    response = {
+        'status': 'ok',
+        'messages': [m.to_dict() for m in paginator.page(page).object_list],
+        'totalPages': paginator.num_pages,
+        'totalMessages': paginator.count,
+    }
+
+    return JsonResponse(response)
+
+
+def get_bros(request):
+    """Returns bros that the current user have"""
+    user = request.user  # TODO make sure the user is token authnticated
+    bros = user.userprofile.bros.all()
+
+    response = {
+        'status': 'ok',
+        'bros': [bro.to_dict() for bro in bros],
+    }
+
+    return JsonResponse(response)
+
+
+def get_dashboard(request):
+    """Returns all the data for the dashboard"""
+    user = request.user
+    response = {
+        'totalMessages': Message.objects.filter(receiver=user).count(),
+        'totalMessagesToday': Message.get_messages_from_last_day().count(),
+        'totalMessagesLastWeek': Message.get_messages_from_last_week().count(),
+        'totalBros': user.userprofile.bros.count(),
+    }
+
+    return JsonResponse(response)
